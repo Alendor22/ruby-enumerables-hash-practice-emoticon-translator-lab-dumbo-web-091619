e8f8d0fca5c1022a87c47ee15ef31867dea5b133
Adeja Lendor <alendor6@gmail.com>
2019-09-03 00:22:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@ def load_library(path)
   emo_file = YAML.load_file(path)
   out = {get_emoticon: {}, get_meaning: {}}
   emo_file.each do |key, val|
-    out[get_emoticon][val[0]]
+    out[get_emoticon][val[0]] = val1
   
 end
 
